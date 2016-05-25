2e2afff2b8693120e8a128ed4b78b3cc62adccff
Kyle Luck <luck.kyle@gmail.com>
2016-05-25 13:05:52 -0400

    putting ajax call for results into function

--- movieapp.js
+++ movieapp.js
@@ -6,72 +6,18 @@ $(function() {
     event.preventDefault();
     $('#results').html('');
     page = 1;
-    var search = $(".searchbox").val();
-    $.ajax({
-      url: 'http://omdbapi.com/',
-      method: 'GET',
-      data: {
-        s: search,
-        page: page
-      },
-      success: function(data) {
-        var theCorrectIMDBUrl = "http://imdb.com/title/";
-        var arrayOfResults = data.Search;
-        for (var i = 0; i < arrayOfResults.length; i++) {
-          var arrayElement = arrayOfResults[i];
-          var a = $('<a>')
-            .attr('href', theCorrectIMDBUrl + arrayElement.imdbID);
-          a.data('imdbID', arrayElement.imdbID);
-          var img = $('<img>')
-            .attr('src', arrayElement.Poster)
-            .attr('alt', arrayElement.Title);
-          a.append(img);
-          $('#results').append(a);
-        }
-        if (data.totalResults > data.Search.length) {
-          $('#more-button').show();
-        }
-        console.log('Got the data: ', data);
-      }
-    });
+    callForResultsPage();
   });
+
   $('#more-button').click(function() {
     page++;
-    var search = $(".searchbox").val();
-    $.ajax({
-      url: 'http://omdbapi.com/',
-      method: 'GET',
-      data: {
-        s: search,
-        page: page
-      },
-      success: function(data) {
-        var theCorrectIMDBUrl = "http://imdb.com/title/";
-        var arrayOfResults = data.Search;
-        for (var i = 0; i < arrayOfResults.length; i++) {
-          var arrayElement = arrayOfResults[i];
-          var a = $('<a>')
-            .attr('href', theCorrectIMDBUrl + arrayElement.imdbID);
-          //hopefully this works
-          a.data('imdbID', arrayElement.imdbID);
-          var img = $('<img>')
-            .attr('src', arrayElement.Poster)
-            .attr('alt', arrayElement.Title);
-          a.append(img);
-          $('#results').append(a);
-        }
-        if (data.totalResults > data.Search.length) {
-          $('#more-button').show();
-        }
-        console.log('Got the data: ', data);
-      }
-    });
+    callForResultsPage();
   });
+
   $('body').on('click', 'a', function (event){
     event.preventDefault();
     $('#search-page').hide();
     var imdbID = $(this).data('imdbID');
-    console.log(imdbID);
     //make ajax call
     $.ajax({
       url: 'http://omdbapi.com/',
@@ -96,4 +42,34 @@ $(function() {
     $('#search-page').show();
     $('#details-page').hide();
   });
+
+  function callForResultsPage() {
+    var search = $(".searchbox").val();
+    $.ajax({
+      url: 'http://omdbapi.com/',
+      method: 'GET',
+      data: {
+        s: search,
+        page: page
+      },
+      success: function(data) {
+        var theCorrectIMDBUrl = "http://imdb.com/title/";
+        var arrayOfResults = data.Search;
+        for (var i = 0; i < arrayOfResults.length; i++) {
+          var arrayElement = arrayOfResults[i];
+          var a = $('<a>');
+          a.data('imdbID', arrayElement.imdbID);
+          var img = $('<img>')
+            .attr('src', arrayElement.Poster)
+            .attr('alt', arrayElement.Title);
+          a.append(img);
+          $('#results').append(a);
+        }
+        if (data.totalResults > data.Search.length) {
+          $('#more-button').show();
+        }
+        //console.log('Got the data: ', data);
+      }
+    });
+  }
 });
